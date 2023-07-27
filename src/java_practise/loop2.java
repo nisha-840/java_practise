3bdbe6540f72efa312a5dd85d9d4b7def5b66601
[Nisha Tellis] <[tellisnisha@gmail.com]>
2023-07-27 12:11:57 +0530

    first java practise file

---
+++ src/java_practise/loop2.java
@@ -0,0 +1,10 @@
+package java_practise;
+
+public class loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello im a dummy file");
+	}
+
+}
