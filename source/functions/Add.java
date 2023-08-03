516f397972b2942a74cde50fae8ed978624a215e
[Nisha Tellis] <[tellisnisha@gmail.com]>
2023-08-03 19:20:56 +0530

    updated

---
+++ source/functions/Add.java
@@ -0,0 +1,20 @@
+package functions;
+
+public class Add {
+	
+	
+	public static int add(int a, int b) {
+		int sum=a+b;
+		return sum;
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9,b=12;
+		int sum=add(a,b);
+		System.out.println(sum);
+	}
+
+}
