6039434bca4746af05ff318a830e25aca515ddf6
[Nisha Tellis] <[tellisnisha@gmail.com]>
2023-08-03 19:05:29 +0530

    new source file

---
+++ source/patterns/solid_rectangle.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class solid_rectangle {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		int m=4;
+		int n=4;
+		for(i=1;i<=n;i++) {
+			for(j=1;j<=m;j++) {
+				System.out.print("*");
+			}System.out.println("");
+		}
+		
+	}
+}
+
